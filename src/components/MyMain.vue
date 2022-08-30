ff81c9fc28d92b97a1aca2714a9384bffa937f48
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-08-30 18:25:28 +0200

    added v-if in case .poster_path key == null
    displays a default image

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,9 +1,14 @@
 <template>
   <main>
+    <h1>MOVIES</h1>
     <div id="movies-container">
-        <h1>MOVIES</h1>
-        <ul v-for="(movie, index) in movies" :key="index">
+        
+        <ul class="list" v-for="(movie, index) in movies" :key="index">
             <li>
+                <div class="no-poster" v-if="movie.poster_path == null">
+                    <img src="../assets/images/noposter.svg" alt="">
+                </div>
+                <img v-else :src="posterURL + movie.poster_path" alt="">
                 <h4>{{movie.title}}</h4>
                 <h5>{{movie.original_title}}</h5>
                 <div v-if="movie.original_language == 'en'">
@@ -18,10 +23,15 @@
             </li>
         </ul>
     </div>
-    <div class="tvseries-container">
-        <h1>TV SERIES</h1>
-        <ul v-for="(tvseries, index) in tvSeries" :key="index">
+    <h1>TV SERIES</h1>
+    <div id="tvseries-container">
+        
+        <ul class="list" v-for="(tvseries, index) in tvSeries" :key="index">
             <li>
+                <div class="no-poster" v-if="tvseries.poster_path == null">
+                    <img src="../assets/images/noposter.svg" alt="">
+                </div>
+                <img v-else :src="posterURL + tvseries.poster_path" alt="">
                 <h4>{{tvseries.title}}</h4>
                 <h5>{{tvseries.original_name}}</h5>
                 <div v-if="tvseries.original_language == 'en'">
@@ -51,7 +61,7 @@ export default {
     name: 'MyMain',
     data() {
         return {
-            
+            posterURL: 'https://image.tmdb.org/t/p/w342'
         }
     },
     props: {
@@ -67,4 +77,36 @@ export default {
     .w-20 {
         width: 20px;
     }
+
+    ul {
+        min-width: 342px;
+        list-style: none;
+    }
+
+    #movies-container, #tvseries-container {
+        
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+
+        li {
+            width: 100%;
+        }
+    }
+
+    .no-poster {
+        width: 100%;
+        height: 513px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #ccc;
+
+        img {
+            width: 60%;
+        }
+    }
+
+    
 </style>
