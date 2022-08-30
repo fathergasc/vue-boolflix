2238bdb2e82468d9611fdafbaf9d3d604ef834dc
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-08-30 17:28:27 +0200

    added gb/it flags for original_language key

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -4,8 +4,15 @@
         <li>
             <h4>{{movie.title}}</h4>
             <h5>{{movie.original_title}}</h5>
-            <h5>{{movie.original_language}}</h5>
+            <div v-if="movie.original_language == 'en'">
+                <img class="w-20" src="../assets/images/gb.svg" alt="">
+            </div>
+            <div v-else-if="movie.original_language == 'it'">
+                <img class="w-20" src="../assets/images/it.svg" alt="">
+            </div>
+            <h5 v-else>{{movie.original_language}}</h5>
             <h5>{{movie.vote_average}}</h5>
+            <img src="" alt="">
         </li>
     </ul>
 
@@ -28,9 +35,27 @@ export default {
     props: {
         movies: Object
     },
+    methods: {
+        getImg(path){
+            return require('../assets/images/' + path);
+        },
+        langToFlag(lang) {
+            switch (lang) {
+                case 'en':
+                return 'gb.svg';
+                case 'it':
+                return 'it.svg';
+                default:
+                return lang;
+                
+            }
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+    .w-20 {
+        width: 20px;
+    }
 </style>
