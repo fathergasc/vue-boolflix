7d9ff289550f63d61cdeb137a6f5d1d287b46468
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-08-30 17:39:48 +0200

    added getTvSeries() in App.vue to search tv series

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <MyHeader @search="getSearch"/>
-    <MyMain :movies="movies"/>
+    <MyMain :movies="movies" :tvSeries="tvSeries"/>
   </div>
 </template>
 
@@ -23,20 +23,29 @@ export default {
       search: '',
       apiKey: 'cea5cb6798a2c2b6c1fac1af952cc7a4',
       moviesEndpoint:'https://api.themoviedb.org/3/search/movie?api_key=',
-      movies: {},
+      movies: [],
+      tvSeries: [],
+      tvSeriesEndpoint: 'https://api.themoviedb.org/3/search/tv?api_key='
     }
   },
   methods: {
     getSearch(search) {
       this.search = search;
       this.getMovies();
+      this.getTvSeries();
     },
     getMovies() {
             axios.get(this.moviesEndpoint + this.apiKey + '&query=' + this.search + '&language=it-IT')
                 .then((response) => {
                     this.movies = response.data.results;
                 })
-        }
+        },
+    getTvSeries() {
+            axios.get(this.tvSeriesEndpoint + this.apiKey + '&query=' + this.search + '&language=it-IT')
+                .then((response) => {
+                    this.tvSeries = response.data.results;
+                })
+        },
   }
 }
 </script>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,22 +1,44 @@
 <template>
-  <div>
-    <ul v-for="(movie, index) in movies" :key="index">
-        <li>
-            <h4>{{movie.title}}</h4>
-            <h5>{{movie.original_title}}</h5>
-            <div v-if="movie.original_language == 'en'">
-                <img class="w-20" src="../assets/images/gb.svg" alt="">
-            </div>
-            <div v-else-if="movie.original_language == 'it'">
-                <img class="w-20" src="../assets/images/it.svg" alt="">
-            </div>
-            <h5 v-else>{{movie.original_language}}</h5>
-            <h5>{{movie.vote_average}}</h5>
-            <img src="" alt="">
-        </li>
-    </ul>
+  <main>
+    <div id="movies-container">
+        <h1>MOVIES</h1>
+        <ul v-for="(movie, index) in movies" :key="index">
+            <li>
+                <h4>{{movie.title}}</h4>
+                <h5>{{movie.original_title}}</h5>
+                <div v-if="movie.original_language == 'en'">
+                    <img class="w-20" src="../assets/images/gb.svg" alt="">
+                </div>
+                <div v-else-if="movie.original_language == 'it'">
+                    <img class="w-20" src="../assets/images/it.svg" alt="">
+                </div>
+                <h5 v-else>{{movie.original_language}}</h5>
+                <h5>{{movie.vote_average}}</h5>
+                <img src="" alt="">
+            </li>
+        </ul>
+    </div>
+    <div class="tvseries-container">
+        <h1>TV SERIES</h1>
+        <ul v-for="(tvseries, index) in tvSeries" :key="index">
+            <li>
+                <h4>{{tvseries.title}}</h4>
+                <h5>{{tvseries.original_name}}</h5>
+                <div v-if="tvseries.original_language == 'en'">
+                    <img class="w-20" src="../assets/images/gb.svg" alt="">
+                </div>
+                <div v-else-if="tvseries.original_language == 'it'">
+                    <img class="w-20" src="../assets/images/it.svg" alt="">
+                </div>
+                <h5 v-else>{{tvseries.original_language}}</h5>
+                <h5>{{tvseries.vote_average}}</h5>
+                <img src="" alt="">
+            </li>
+        </ul>
+    </div>
+    
 
-  </div>
+  </main>
 </template>
 
 <script>
@@ -33,23 +55,10 @@ export default {
         }
     },
     props: {
-        movies: Object
+        movies: Array,
+        tvSeries: Array
     },
     methods: {
-        getImg(path){
-            return require('../assets/images/' + path);
-        },
-        langToFlag(lang) {
-            switch (lang) {
-                case 'en':
-                return 'gb.svg';
-                case 'it':
-                return 'it.svg';
-                default:
-                return lang;
-                
-            }
-        }
     }
 }
 </script>
